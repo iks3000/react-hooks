7157c6ddd23d97ca5c3fd8c759793e9623d151f6
Stanislav <stanislav.khomyn@skylum.com>
2020-05-05 17:48:27 -0700

    use-effect fetch

--- src/index.js
+++ src/index.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 
 const App = () => {
+  const [value, setValue] = useState(1);
+  const [visible, setVisible] = useState(true);
 
-    return <p>Hello</p>
+  if (visible) {
+    return (
+      <div className="mt-5">
+        <button className="btn btn-primary btn-sm mr-3" onClick={() => setValue((v) => v + 1)}>+</button>
+        <button className="btn btn-primary btn-sm" onClick={() => setVisible(false)}>hide</button>
+        <PlanetInfo id={value} />
+      </div>
+    )
+  } else {
+    return <button className="btn btn-primary btn-sm mt-5" onClick={() => setVisible(true)}>show</button>
+  }
 };
+
+const PlanetInfo = ({ id }) => {
+
+  const loading = (
+    <div class="spinner-border spinner-border-sm text-success" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  )
+
+  const [planet, setPlanet] = useState(loading)
+
+  useEffect(() => {
+    let cancelled = false;
+    fetch(`https://swapi.dev/api/planets/${ id }`)
+      .then(res => res.json())
+      .then(data => !cancelled && setPlanet(data.name));
+    return () => cancelled = true;
+  }, [id])
+
+
+
+  return (
+    <div className="mt-3">{id} - {planet}</div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
